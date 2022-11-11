74e85d37bd5c0110f669c8ed11ef1183a54efdd4
Lukas Kipfer <lukas.kipfer@students.fhnw.ch>
2022-11-11 15:25:37 +0100

    Spring Framework

---
+++ timechamps-backend/src/main/java/ch/fhnw/timechamps/TimechampsApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.timechamps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimechampsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimechampsApplication.class, args);
+	}
+
+}
---
+++ timechamps-backend/src/test/java/ch/fhnw/timechamps/TimechampsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.timechamps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimechampsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
