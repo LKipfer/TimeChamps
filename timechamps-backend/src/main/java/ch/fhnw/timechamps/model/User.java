a4825fbfc9df48b5f50cb9711b611062ce94dde2
Lukas Kipfer <lukas.kipfer@students.fhnw.ch>
2022-11-13 19:10:32 +0100

    Added User class + UserType enum + UserStatus enum

--- timechamps-backend/src/main/java/ch/fhnw/timechamps/model/User.java
+++ timechamps-backend/src/main/java/ch/fhnw/timechamps/model/User.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.nio.charset.StandardCharsets;
 
 
 /**
