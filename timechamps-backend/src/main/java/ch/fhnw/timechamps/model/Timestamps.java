a7779ab16d969b413980f342f36116291093a67b
Lukas Kipfer <lukas.kipfer@students.fhnw.ch>
2022-11-13 13:25:04 +0100

    Added Timestamps class and first methods

--- timechamps-backend/src/main/java/ch/fhnw/timechamps/model/Timestamps.java
+++ timechamps-backend/src/main/java/ch/fhnw/timechamps/model/Timestamps.java
@@ -13,10 +13,50 @@ public class Timestamps {
 
     Instant end = Instant.now();
 
+    Long totalTime;
 
-    public Duration calculateWorktime () {
+
+
+    public Long getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(Long totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public Duration calculateTimestamp () {
         Duration total = (Duration.between(this.start, this.end));
         return total;
+    }
+
+    public Long updateTimestamp () {
+        totalTime = totalTime + this.totalTime;
+        return totalTime;
+    }
+
+    public void parseDuration () {
+
+        /**
+         * source: https://www.geeksforgeeks.org/duration-parsecharsequence-method-in-java-with-examples/
+         * usage: incase we need to parse existing time entries for editing or calculation purposes
+         */
+
+        // Get the text → Needs to be automated!
+        String time = "P2DT3H4M";
+
+        // Duration using parse() method
+        try {
+            Duration duration
+                    = Duration.parse(time);
+
+            System.out.println(duration.getSeconds());
+        }
+
+        catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+
     }
 
 }
