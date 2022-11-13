60da83c09cf9a775982d5e0f0c62b014a91a3298
Lukas Kipfer <lukas.kipfer@students.fhnw.ch>
2022-11-13 11:08:34 +0100

    Added Timestamps class, still needs improvements

--- timechamps-backend/src/main/java/ch/fhnw/timechamps/model/Timestamps.java
+++ timechamps-backend/src/main/java/ch/fhnw/timechamps/model/Timestamps.java
@@ -3,19 +3,22 @@ package ch.fhnw.timechamps.model;
 import java.time.Duration;
 import java.time.Instant;
 
+/**
+ * @author Lukas Kipfer
+ */
+
 public class Timestamps {
 
     Instant start = Instant.now();
 
-
-
-
-        System.out.println("Start Time = " + start);
-        System.out.println("Timer started, please enter any input to stop timer.");
-
     Instant end = Instant.now();
-    Duration timeElapsed = Duration.between(start, end);
-        System.out.println("End Time = " + end);
-        System.out.println("Time taken: "+ timeElapsed.toMillis() +" milliseconds");
+
+
+    public Duration calculateWorktime () {
+        Duration total = (Duration.between(this.start, this.end));
+        return total;
+    }
 
 }
+
+
