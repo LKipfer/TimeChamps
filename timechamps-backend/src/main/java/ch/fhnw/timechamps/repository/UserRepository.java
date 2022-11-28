98e4eb66bbe52983141bfd7c13c33364ed5e0637
Lukas Kipfer <lukas.kipfer@students.fhnw.ch>
2022-11-28 16:06:48 +0100

    added UserRepository

---
+++ timechamps-backend/src/main/java/ch/fhnw/timechamps/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package ch.fhnw.timechamps.repository;
+
+import ch.fhnw.timechamps.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+/**
+ * @author Lukas Kipfer
+ */
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    void deleteUserById(Long userId);
+
+    //The reason why we used "Optional" instead of only "Employee" is for cases, that we do not find an Employee.
+    Optional<User> findUserById(Long userId);
+
+}
