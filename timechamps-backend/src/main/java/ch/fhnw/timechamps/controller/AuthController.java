ff075f153ef3a0acd43937b8e68b7f17c6332d80
Lukas Kipfer <lukas.kipfer@students.fhnw.ch>
2022-12-16 11:19:41 +0100

    fixed forbidden message if already logged in

--- timechamps-backend/src/main/java/ch/fhnw/timechamps/controller/AuthController.java
+++ timechamps-backend/src/main/java/ch/fhnw/timechamps/controller/AuthController.java
@@ -29,14 +29,15 @@ public class AuthController {
 
     @PostMapping("/authenticate")
     public ResponseEntity<String> authenticate(
-            @RequestBody AuthenticationRequest request
-    ) { // The whole authentication process gets delegated to Spring
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-        );
+            @RequestBody AuthenticationRequest request // The whole authentication process gets delegated to Spring
+    ) {
         final UserDetails user = userDao.findUserByEmail(request.getEmail());
         if (user != null) {
             return ResponseEntity.ok(jwtUtils.generateToken(user));
+        } else {
+                authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+            );
         }
         return ResponseEntity.status(400).body("Some error has occurred");
     }
