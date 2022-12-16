63e94ddbc7fd954073b9fe0b4b2f4ff713a16730
Lukas Kipfer <lukas.kipfer@students.fhnw.ch>
2022-12-16 11:00:06 +0100

    changed /auth path to /login

--- timechamps-backend/src/main/java/ch/fhnw/timechamps/controller/AuthController.java
+++ timechamps-backend/src/main/java/ch/fhnw/timechamps/controller/AuthController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/login")
 @RequiredArgsConstructor
 public class AuthController {
 
